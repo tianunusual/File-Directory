f5500118d83831641e1915a92993c93037ab9aad
王不留行 <106044126+tianunusual@users.noreply.github.com>
2022-11-30 09:34:08 +0800

    Delete _dir  /data directory

--- _dir  /data/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:15:{s:14:"admin_username";s:5:"admin";s:14:"admin_password";s:32:"e10adc3949ba59abbe56e057f20f883e";s:5:"title";s:23:"文件目录列表 v1.1";s:8:"keywords";s:65:"文件目录列表 v1.1,Directory Lister目录列表,目录索引";s:11:"description";s:23:"文件目录列表 v1.1";s:8:"announce";s:0:"";s:6:"footer";s:0:"";s:11:"name_encode";s:4:"utf8";s:9:"file_hash";s:1:"1";s:13:"cache_indexes";s:1:"0";s:9:"readme_md";s:1:"1";s:4:"auth";s:1:"0";s:3:"nav";s:98:"Github*https://github.com/tianunusual|Adultle*https://Adultle.com/|TG Group*https://t.me/adultdow/";s:13:"admin_session";s:32:"82647be0570045933c94ba6f5c07aaeb";s:15:"admin_lastlogin";s:19:"2022-11-30 07:32:59";}
